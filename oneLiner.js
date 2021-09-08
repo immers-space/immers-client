1e0c4b631e446d6e4b10cfac8ec65ce2fe98090f
Will Murphy <william@datatitian.com>
2021-09-08 01:15:19 -0500

    only insert HUD if one isn't already present

--- oneLiner.js
+++ oneLiner.js
@@ -13,13 +13,26 @@ try {
     // token was passed to opener window; this is just a popup
     return
   }
-  if (scriptArgs.position !== 'none') {
-    const hud = document.createElement('immers-hud')
-    hud.setAttribute('position', scriptArgs.position ?? 'bottom-left')
-    hud.setAttribute('destination-name', document.title)
-    hud.setAttribute('destination-url', window.location.href)
-    hud.setAttribute('token-catcher', window.location.href)
-    hud.setAttribute('access-role', scriptArgs.role ?? roles[1])
-    document.body.appendChild(hud)
+  const installHUD = () => {
+    // insert the hud element unless disabled by script arg or already present
+    if (scriptArgs.position !== 'none' && !document.querySelector('immers-hud')) {
+      const hud = document.createElement('immers-hud')
+      hud.setAttribute('position', scriptArgs.position ?? 'bottom-left')
+      hud.setAttribute('destination-name', document.title)
+      hud.setAttribute('destination-url', window.location.href)
+      hud.setAttribute('token-catcher', window.location.href)
+      hud.setAttribute('access-role', scriptArgs.role ?? roles[1])
+      document.body.appendChild(hud)
+    }
+  }
+
+  if (/complete|interactive|loaded/.test(document.readyState)) {
+    installHUD()
+  } else {
+    document.addEventListener('readystatechange', () => {
+      if (document.readyState === 'interactive') {
+        installHUD()
+      }
+    }, { once: true })
   }
 })()
